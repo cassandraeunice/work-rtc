278c2065a56cbe3b02f0b2104709833925235393
orayanics <nicole.oraya.cics@ust.edu.ph>
2024-07-15 21:24:21 +0800

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -17,11 +17,11 @@ const serverConfig = {
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../../client/build')));
 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
 
 // Start the HTTPS Server
